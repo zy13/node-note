ff6274bf7a85b67206c0e99668879cc7ad462bea
zy <zengyuan@keywa.com>
2021-02-24 09:33:57 +0800

    nunjucks模板引擎的使用
    进行数据和视图分离

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// 引入http模块
-const http = require('http')
-// 引入fs模块：对文件或目录进行增删改查，以及监听它们的变化
-const fs = require('fs')
-// 引入mime
-const mimes = require('./mime.json')
-// 创建http.Sever对象
-const server = http.createServer()
+const http = require('http') // 引入http模块
+const fs = require('fs') // 引入fs模块
+const mimes = require('./mime.json') // 引入mime
+const nunjucks = require('nunjucks') // 引入模板引擎
+
+// let res = nunjucks.renderString('hello {{name}}', {name: 'james'})
+// console.log(res)
+
+// 渲染模板文件，进行模板配置
+nunjucks.configure('views', {
+  autoescape: true,
+  watch: true,
+  noCache: true // 开发过程不需要做环境
+})
+
+const server = http.createServer() // 创建http.Sever对象
+
+let users = [
+  {id: 1, name: 'zy1'},
+  {id: 2, name: 'zy2'}
+]
 
 // 注册请求回调函数
 server.on('request', (req, res) => {
@@ -36,6 +49,16 @@ server.on('request', (req, res) => {
         'content-type': 'text/html;charset=utf-8'
       })
       content = `系统时间：${new Date()}`
+    }
+    if(url === '/users') {
+      res.writeHead(200,{
+        'content-type': 'text/html;charset=utf-8'
+      })
+      content = nunjucks.render('users.html', {
+        title: '用户列表',
+        users
+      })
+      console.log(content)
     }
   }  
 
